778f15552ee05a050cf184be61c354c124afeae6
modernator <rico345100@gmail.com>
2018-10-19 12:38:41 +0900

    Added custom events

--- app/js/app.js
+++ app/js/app.js
@@ -2,3 +2,12 @@ var el = document.createElement('custom-list');
 document.body.appendChild(el);
 
 el.setAttribute('data', [1,2,3,4,5].join(','));
+
+el.addEventListener('onrendered', (ev) => {
+    console.log('<custom-list />::render');
+});
+
+el.addEventListener('onattributechanged', (ev) => {
+    const { attrName, newVal, oldVal } = ev.detail;
+    console.log(ev.detail);
+});
--- app/js/custom-list.js
+++ app/js/custom-list.js
@@ -11,7 +11,6 @@ class CustomList extends HTMLElement {
         super(...args);
         this.attachShadow({ mode: 'open' });
         const containerEl = document.createElement('div');
-
         this.shadowRoot.appendChild(containerEl);
     }
     connectedCallback() {
@@ -19,32 +18,42 @@ class CustomList extends HTMLElement {
     }
     attributeChangedCallback(attrName, oldVal, newVal) {
         this.render();
+
+        const ev = new CustomEvent('onattributechanged', { detail: { attrName, oldVal, newVal }});
+        this.dispatchEvent(ev);
     }
     render() {
-        const data = this.getAttribute('data');
-        const containerEl = this.shadowRoot.querySelector('div');
-        const childs = Array.prototype.slice.call(containerEl.childNodes);
-        
-        for(let i = 0; i < childs.length; i++) {
-            containerEl.removeChild(childs[i]);
-        }
-        
-        if(data) { 
-            const splitedData = data.trim().split(",");
+        // Delay the render 0 ms
+        setTimeout(() => {
+            const data = this.getAttribute('data');
+            const containerEl = this.shadowRoot.querySelector('div');
+            const childs = Array.prototype.slice.call(containerEl.childNodes);
+            
+            for(let i = 0; i < childs.length; i++) {
+                containerEl.removeChild(childs[i]);
+            }
+            
+            if(data) { 
+                const splitedData = data.trim().split(",");
 
-            splitedData.map(data => {
-                const divEl = document.createElement('div');
+                splitedData.map(data => {
+                    const divEl = document.createElement('div');
 
-                if(divEl.textContent) {
-                    divEl.textContent = `Data: ${data}`;
-                }
-                else {
-                    divEl.innerText = `Data: ${data}`;
-                }
+                    if(divEl.textContent) {
+                        divEl.textContent = `Data: ${data}`;
+                    }
+                    else {
+                        divEl.innerText = `Data: ${data}`;
+                    }
 
-                containerEl.appendChild(divEl);
-            });
-        }
+                    containerEl.appendChild(divEl);
+                });
+            }
+
+            // Invoke Render Event
+            const ev = new CustomEvent('onrendered');
+            this.dispatchEvent(ev);
+        });
     }
 }
 
